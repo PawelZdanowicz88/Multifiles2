6edd1547caeeabc029a682377342f1e56fd4a40e
PawelZdanowicz88 <zdanowiczpawel88@gmail.com>
2022-11-06 12:38:05 +0100

    Recorded files added

--- Multifile2/Header.h
+++ Multifile2/Header.h
@@ -3,21 +3,9 @@ class Counter {
 private:
     int value;
 public:
-    Counter(int v1) :
-        value(v1)
-    {
-    }
-    Counter() :
-        value(1)
-    {
-    }
-    void increase() {
-        value = value + 1;
-    }
-    void decrease() {
-        value = value - 1;
-    }
-    int present() {
-        return value;
-    }
+    Counter(int v1);
+    Counter();
+    void increase();
+    void decrease();
+    int present();
 };
--- Multifile2/Source.cpp
+++ Multifile2/Source.cpp
@@ -1,2 +1,19 @@
 #include "Header.h"
 
+    Counter::Counter(int v1) :
+        value(v1)
+    {
+    }
+    Counter::Counter() :
+        value(1)
+    {
+    }
+    void Counter::increase() {
+        value = value + 1;
+    }
+    void Counter::decrease() {
+        value = value - 1;
+    }
+    int Counter::present() {
+        return value;
+    }
